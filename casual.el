7176489746574e84d7865c555c1fb408c53014e7
Charles Choi <kickingvegas@gmail.com>
2024-03-07 15:37:07 -0800

    Bump to 1.0.3

--- casual.el
+++ casual.el
@@ -5,7 +5,7 @@
 ;; Author: Charles Choi <kickingvegas@gmail.com>
 ;; URL: https://github.com/kickingvegas/casual
 ;; Keywords: tools
-;; Version: 1.0.2
+;; Version: 1.0.3
 ;; Package-Requires: ((emacs "29.1"))
 
 ;; This program is free software; you can redistribute it and/or modify
