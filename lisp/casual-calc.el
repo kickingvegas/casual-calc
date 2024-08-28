097dc587fd8fd4f3cb9e9e7354f0b157e2fc5297
Charles Choi <kickingvegas@gmail.com>
2024-08-27 21:30:48 -0700

    Copy Edits
    - Updated install instructions to discuss updating built-in packages.
    - Updated "See Also" section.
    - Updated "Requirements" section.

--- lisp/casual-calc.el
+++ lisp/casual-calc.el
@@ -43,6 +43,12 @@
 ;;          ("C-o" . casual-calc-tmenu))
 ;;   :after (calc))
 
+;; NOTE: This package requires `casual-lib' which in turn requires an update of
+;; the built-in package `transient' ≥ 0.6.0. Please customize the variable
+;; `package-install-upgrade-built-in' to t to allow for `transient' to be
+;; updated. For further details, consult the INSTALL section of this package's
+;; README.
+
 ;;; Code:
 
 (require 'calc)
